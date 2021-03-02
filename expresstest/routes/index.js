4caaec6096128bf2c126ee98b71a84666b2fe81e
yuemw <ymw_mail@163.com>
2021-03-02 16:20:03 +0800

    【提交者】yuemw 【提交原因】路由测试学习
    Change-Id: I6467717e5ed3637b3da3b2d83b40f73389b34224

--- expresstest/routes/index.js
+++ expresstest/routes/index.js
@@ -1,9 +1,31 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: lhl
+ * @Date: 2020-07-17 21:17:15
+ * @LastEditors: lhl
+ * @LastEditTime: 2021-02-24 18:38:50
+ */
 var express = require('express');
+var fs = require('fs');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('F:/program/js/expresstest/public/html/home.html', function (err, data) {
+    if (err) {
+       console.log(err)
+       res.writeHead(404, {'Content-Type': 'text/html'});
+    }else{             
+       res.writeHead(200, {'Content-Type': 'text/html'});    
+       // 响应文件内容
+       res.write(data.toString());        
+    }
+    //  发送响应数据
+    res.end();
+  }); 
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

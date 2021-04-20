a324b9491a2ec85f41ade359056c89c8dc6385c6
yuemw <ymw_mail@163.com>
2021-04-20 13:10:41 +0800

    【提交者】yuemw 【提交原因】网络通信测试代码调整
    Change-Id: Ib884d3972362d141349b91b492ae0458886deb68

--- communicationtest/index.js
+++ communicationtest/index.js
@@ -1,2 +1,12 @@
+/*
+ * @Description: 网络通信测试入口
+ * @Version: 2.0
+ * @Autor: ymw
+ * @Date: 2020-07-17 11:08:06
+ * @LastEditors: ymw
+ * @LastEditTime: 2021-04-20 11:47:00
+ */
 //入口文件
-require('./webservice/webserveice');
+// require('./webservice/webserveice');
+// require('./http/httpserver');
+require('./tcp/server');
